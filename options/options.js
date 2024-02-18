43994733eaabb7ff6dc38901e30f64bc025007b9
Crowdbotics <team@crowdbotics.com>
2024-02-18 21:33:16 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quiet-wildflower-46452",
+  url: "https://quiet-wildflower-46452.botics.co",
+  api: "https://quiet-wildflower-46452.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
